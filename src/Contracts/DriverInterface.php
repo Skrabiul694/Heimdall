16f7e0eacbaefe95d54ac642241143298bd9906b
Sk Rabiul <105815066+Skrabiul694@users.noreply.github.com>
2026-07-23 20:00:38 +0530

    Update DriverInterface.php

--- src/Contracts/DriverInterface.php
+++ src/Contracts/DriverInterface.php
@@ -1,1 +1,11 @@
+<?php
 
+namespace Vendor\Heimdall\Contracts;
+
+use Vendor\Heimdall\DataTransferObjects\PromptRequest;
+use Vendor\Heimdall\DataTransferObjects\ProviderResponse;
+
+interface DriverInterface
+{
+    public function send(PromptRequest $request): ProviderResponse;
+}
