77f43c263f428b1d16ea9e9d61b03e511fa1d6e9
Sk Rabiul <105815066+Skrabiul694@users.noreply.github.com>
2026-07-23 20:02:23 +0530

    Update BaseDriver.php

--- src/drivers/BaseDriver.php
+++ src/drivers/BaseDriver.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace Vendor\Heimdall\Drivers;
+
+use Vendor\Heimdall\Contracts\DriverInterface;
+
+abstract class BaseDriver implements DriverInterface
+{
+    protected array $config;
+    protected int $timeout;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+        $this->timeout = config('heimdall.timeout', 30);
+    }
+}
