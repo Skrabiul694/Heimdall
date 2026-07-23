de88b6cc378aea9d7fbeabe0145cd28ce7e60cb1
Sk Rabiul <105815066+Skrabiul694@users.noreply.github.com>
2026-07-23 20:04:19 +0530

    Update Heimdall.php

--- src/Facades/Heimdall.php
+++ src/Facades/Heimdall.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Vendor\Heimdall\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use Vendor\Heimdall\DataTransferObjects\PromptRequest;
+use Vendor\Heimdall\DataTransferObjects\ProviderResponse;
+
+/**
+ * @method static ProviderResponse executeWithFailover(PromptRequest $request, array $pipeline)
+ * @method static \Vendor\Heimdall\Contracts\DriverInterface driver(string|null $driver = null)
+ * 
+ * @see \Vendor\Heimdall\HeimdallManager
+ */
+class Heimdall extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return 'heimdall';
+    }
+}
