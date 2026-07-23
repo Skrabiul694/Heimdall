b7c8a201d1e272ceec7bc6b0be727eac9611d837
Sk Rabiul <105815066+Skrabiul694@users.noreply.github.com>
2026-07-23 20:02:07 +0530

    Update LlmLog.php

--- src/Models/LlmLog.php
+++ src/Models/LlmLog.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace Vendor\Heimdall\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LlmLog extends Model
+{
+    protected $guarded = [];
+
+    protected $casts = [
+        'raw_payload' => 'array',
+        'is_success' => 'boolean',
+    ];
+}
