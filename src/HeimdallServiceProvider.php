4c4ef731f0d08d10e3c9357d747b9335b6f5b1ab
Sk Rabiul <105815066+Skrabiul694@users.noreply.github.com>
2026-07-23 20:04:02 +0530

    Update HeimdallServiceProvider.php

--- src/HeimdallServiceProvider.php
+++ src/HeimdallServiceProvider.php
@@ -1,1 +1,34 @@
+<?php
 
+namespace Vendor\Heimdall;
+
+use Illuminate\Support\ServiceProvider;
+
+class HeimdallServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/heimdall.php', 'heimdall'
+        );
+
+        $this->app->singleton('heimdall', function ($app) {
+            return new HeimdallManager($app);
+        });
+    }
+
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/heimdall.php' => config_path('heimdall.php'),
+            ], 'heimdall-config');
+
+            $this->publishes([
+                __DIR__ . '/../database/migrations' => database_path('migrations'),
+            ], 'heimdall-migrations');
+        }
+    }
+}
